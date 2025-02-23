8cd13f3fa7dccce38e8bf60f31122bde5e5134a9
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2025-02-23 16:44:24 -0300

    feat: created project

---
+++ src/main/java/com/gabriel_gjs/finance_ctrl/FinanceCtrlApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel_gjs.finance_ctrl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCtrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceCtrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel_gjs/finance_ctrl/FinanceCtrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel_gjs.finance_ctrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceCtrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
