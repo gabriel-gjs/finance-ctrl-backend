dd8417108fbd8f43efec20b7e36c0c99b5b3692d
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2025-03-05 10:45:36 -0300

    feat: the first configuration on Spring Security

---
+++ src/main/java/com/gabriel_gjs/finance_ctrl/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.gabriel_gjs.finance_ctrl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/gabriel_gjs/finance_ctrl/services/AuthorizationService.java
@@ -0,0 +1,19 @@
+package com.gabriel_gjs.finance_ctrl.services;
+
+import com.gabriel_gjs.finance_ctrl.domain.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email);
+    }
+}
