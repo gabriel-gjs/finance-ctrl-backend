18fe0d94ab79a00c362bfef99c1bf9e56b1b4e4c
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2025-03-05 10:45:10 -0300

    refactor: delete entity role

--- src/main/java/com/gabriel_gjs/finance_ctrl/domain/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gabriel_gjs.finance_ctrl.domain.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<RoleRepository,Long> {}
